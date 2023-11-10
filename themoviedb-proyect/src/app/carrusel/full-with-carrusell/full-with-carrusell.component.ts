2c426906b4cd15c66d0b75d88937c3cde0e17a91
MarcoPertegal <marco.pertegal.prieto@gmail.com>
2023-11-10 12:26:53 +0100

    Carrusel sin finalizar

--- themoviedb-proyect/src/app/carrusel/full-with-carrusell/full-with-carrusell.component.ts
+++ themoviedb-proyect/src/app/carrusel/full-with-carrusell/full-with-carrusell.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Movie } from 'src/app/models/movie-list.interface';
 import { MovieService } from 'src/app/services/movie.service';
 
@@ -7,30 +7,14 @@ import { MovieService } from 'src/app/services/movie.service';
   templateUrl: './full-with-carrusell.component.html',
   styleUrls: ['./full-with-carrusell.component.css']
 })
-export class FullWithCarrusellComponent implements OnInit {
-  /*Bootstrapejemplo*/images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+export class FullWithCarrusellComponent {
 
-  //PARA SACAR IMAGEN DEL CACRRUSEL
-  //faltaria hacer el backdrop imagen que se saca de esta url https://image.tmdb.org/t/p/original/f2t4JbUvQIjUF5FstG1zZFAp02N.jpg
-  //la ruta viene en backdrop_path de https://api.themoviedb.org/3/movie/5?api_key=18c6dd9c77bfcc97e862001655abfba9
+  @Input() movie!: Movie;
 
-
-
-  movieList: Movie[] = [];
-
-  constructor(private movieService: MovieService) { }
-
-  ngOnInit(): void {
-    this.loadMovies();
-  }
-
-  loadMovies() {
-    this.movieService.getPopularList().subscribe(resp => {
-      this.movieList = resp.results;
-    })
-  }
+  images = '';
 
   getbackdropImage() {
-    return 'https://image.tmdb.org/t/p/original/${this.movie.poster_path}';
+    this.images = `https://image.tmdb.org/t/p/original/${this.movie.poster_path}`;
   }
+
 }
