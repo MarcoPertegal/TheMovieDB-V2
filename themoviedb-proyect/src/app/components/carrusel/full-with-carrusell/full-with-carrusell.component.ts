7adce4557222234e46ebe3b609dbff757cfd28a6
Fran Ruiz <fjavierrule@gmail.com>
2023-11-15 13:31:10 +0100

    carousel

--- themoviedb-proyect/src/app/components/carrusel/full-with-carrusell/full-with-carrusell.component.ts
+++ themoviedb-proyect/src/app/components/carrusel/full-with-carrusell/full-with-carrusell.component.ts
@@ -8,35 +8,23 @@ import { MovieService } from 'src/app/services/movie.service';
   styleUrls: ['./full-with-carrusell.component.css']
 })
 export class FullWithCarrusellComponent implements OnInit {
-prueba() {
-console.log('funciona')
-}
+  prueba() {
+    console.log('funciona')
+  }
 
-  // movie!: Movie;
-  // movieList: Movie[] = [];
-  // movies: Movie[] = [];
+  movie!: Movie;
+  movieList: Movie[] = [];
+  movies: Movie[] = [];
 
   constructor(private service: MovieService) { }
 
   bannerResult: any = [];
 
   ngOnInit(): void {
-    // this.service.getPopularList().subscribe(resp =>{
-    //   this.movieList = resp.results;
-    //    this.movies = this.movieList.slice(0, 4);
-    //    console.log(this.movies) 
-    // });
-    this.bannerData();
-  }
-
-  bannerData() {
-    this.service.banner().subscribe(resp => {
-      console.log(resp);
-      this.bannerResult = resp.results;
+    this.service.getPopularList().subscribe(resp => {
+      this.movieList = resp.results;
+      this.movies = this.movieList.slice(0, 4);
+      console.log(this.movies)
     });
   }
-  // ImgCarousel() {
-  //   return `https://www.themoviedb.org/t/p/w533_and_h300_bestv2/${this.movie.backdrop_path}`
-  // }
-
 }
