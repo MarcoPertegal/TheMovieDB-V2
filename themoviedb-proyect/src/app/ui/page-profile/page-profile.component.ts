944041350a4951c75f6873be17a0879b34855baf
MarcoPertegal <marco.pertegal.prieto@gmail.com>
2023-11-16 14:19:01 +0100

    ts

--- themoviedb-proyect/src/app/ui/page-profile/page-profile.component.ts
+++ themoviedb-proyect/src/app/ui/page-profile/page-profile.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Movie } from 'src/app/models/movie-list.interface';
+import { AccountService } from 'src/app/services/account.service';
 
 @Component({
   selector: 'app-page-profile',
   templateUrl: './page-profile.component.html',
   styleUrls: ['./page-profile.component.css']
 })
-export class PageProfileComponent {
- active = 1;
-  
+export class PageProfileComponent implements OnInit {
+  active = 1;
+  movieList: Movie[] = [];
+  pageNumber: number = 1;
+  count: number = 0;
+  currentPage: number = 1;
+
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+    this.loadNewPage();
+  }
+  loadNewPage() {
+    this.accountService.getFavorites(this.pageNumber).subscribe(resp => {
+      this.movieList = resp.results;
+      this.count = resp.total_results;
+    })
+  }
+
   getUsername() {
     return localStorage.getItem('USERNAME');
   }
