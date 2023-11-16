236f61994a3364dba886c416edf81f7c343707c6
MarcoPertegal <marco.pertegal.prieto@gmail.com>
2023-11-16 14:17:51 +0100

    service

--- themoviedb-proyect/src/app/services/account.service.ts
+++ themoviedb-proyect/src/app/services/account.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.development';
 import { AccountDetailsResponse } from '../models/account-details.interface';
 import { Observable } from 'rxjs';
+import { MovieListResponse } from '../models/movie-list.interface';
 
 
 @Injectable({
@@ -14,9 +15,23 @@ export class AccountService {
 
   getAccountDetails(): Observable<AccountDetailsResponse> {
     let sessionId = localStorage.getItem('SESSION_ID');
-    return this.http.get<AccountDetailsResponse>(`${environment.baseUrl}/account?session_id=${sessionId}&api_key=${environment.apiKeyMarco}`,
+    return this.http.get<AccountDetailsResponse>(`${environment.baseUrl}/account?session_id=${sessionId}`,
       {
         headers: {
+          accept: 'application/json',
+          'Authorization': `Bearer ${environment.tmdbTokenMarco}`
+        }
+      });
+  }
+
+  getFavorites(page: number): Observable<MovieListResponse> {
+    //api_key=18c6dd9c77bfcc97e862001655abfba9& alomejor no hace falta
+    let sessionId = localStorage.getItem('SESSION_ID');
+    let accountId = localStorage.getItem('ACCOUNT_ID');
+    return this.http.get<MovieListResponse>(`${environment.baseUrl}/account/${accountId}/favorite/movies?session_id=${sessionId}&page=${page}`,
+      {
+        headers: {
+          accept: 'application/json',
           'Authorization': `Bearer ${environment.tmdbTokenMarco}`
         }
       });
